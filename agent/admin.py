f7aa2fd60541df659669b757a83d8bb2d6fc8f2e
“Gbenga <dtechydude@gmail.com>
2025-03-20 21:16:38 +0100

    changed virtual environment and python version

--- agent/admin.py
+++ agent/admin.py
@@ -5,8 +5,8 @@ from import_export.admin import ImportExportModelAdmin
 
 class AgentProfileAdmin(ImportExportModelAdmin, admin.ModelAdmin):
            
-    list_display=('profile__user', 'profile__code', 'profile__gender', 'profile__state')
-    list_filter  = ['profile__state',]
-    search_fields = ('profile__user__username', 'profile__code', 'profile__state')
+    list_display=('profile',)
+    list_filter  = ['profile',]
+    search_fields = ('profile',)
 
 admin.site.register(AgentList, AgentProfileAdmin)
--- merchant/admin.py
+++ merchant/admin.py
@@ -5,8 +5,8 @@ from import_export.admin import ImportExportModelAdmin
 
 class MerchantProfileAdmin(ImportExportModelAdmin, admin.ModelAdmin):
            
-    list_display=('profile__user', 'profile__code', 'profile__gender', 'profile__state')
-    list_filter  = ['profile__state',]
-    search_fields = ('profile__user__username', 'profile__code', 'profile__state')
+    list_display=('business_type',)
+    list_filter  = ['profile',]
+    search_fields = ('profile',)
 
 admin.site.register(Merchant, MerchantProfileAdmin)
